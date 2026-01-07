85f053f445ba0aa5fc0d0e2be5b990b5cd56ba3e
Nuša Pukšič <nusa.puksic@gmail.com>
2026-01-07 20:46:17 +0100

    Index: do not index drafts

--- src/Util/IndexableArticleChecker.php
+++ src/Util/IndexableArticleChecker.php
@@ -4,11 +4,22 @@ namespace App\Util;
 
 use App\Entity\Article;
 use App\Enum\IndexStatusEnum;
+use App\Enum\KindsEnum;
 
 class IndexableArticleChecker
 {
     public static function isIndexable(Article $article): bool
     {
-        return $article->getIndexStatus() !== IndexStatusEnum::DO_NOT_INDEX;
+        // Don't index drafts - they're private working copies
+        if ($article->getKind() === KindsEnum::LONGFORM_DRAFT->value) {
+            return false;
+        }
+
+        // Don't index articles explicitly marked as do not index
+        if ($article->getIndexStatus() === IndexStatusEnum::DO_NOT_INDEX) {
+            return false;
+        }
+
+        return true;
     }
 }
