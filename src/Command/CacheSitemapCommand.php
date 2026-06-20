b69850bae12cfd78991a27693db566e1c12d59be
Nuša Pukšič <nusa.puksic@gmail.com>
2026-06-20 10:35:01 +0200

    Newsroom: cache sitemap

--- src/Command/CacheSitemapCommand.php
+++ src/Command/CacheSitemapCommand.php
@@ -14,6 +14,7 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Routing\RouterInterface;
 
 #[AsCommand(
