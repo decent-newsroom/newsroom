c4c27d649d6421d2e5adf7d23618b9a4ddc24391
Nuša Pukšič <nusa.puksic@gmail.com>
2026-06-26 20:42:00 +0200

    Newsroom: remove a broken bundle

--- src/NostrProjectionBundle/Exception/ProjectionException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace DecentNewsroom\NostrProjectionBundle\Exception;
-
-class ProjectionException extends \RuntimeException
-{
-}
--- src/NostrProjectionBundle/Exception/RawEventStoreException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace DecentNewsroom\NostrProjectionBundle\Exception;
-
-final class RawEventStoreException extends ProjectionException
-{
-}
