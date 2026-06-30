ebac5c42cc3fd12cf3ff74d51ed1b5792b5661d5
Nuša Pukšič <nusa.puksic@gmail.com>
2026-06-30 09:49:26 +0200

    Newsroom: action strip

---
+++ src/Twig/Components/Molecules/ArticleSocialActions.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig\Components\Molecules;
+
+use App\Entity\Article;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent('Molecules:ArticleSocialActions')]
+final class ArticleSocialActions
+{
+    public Article $article;
+
+    public string $coordinate = '';
+
+    public string $canonicalUrl = '';
+
+    public string $naddrEncoded = '';
+
+    public string $commentFormId = 'article-comment-form';
+
+    public ?string $recipientLud16 = null;
+
+    public ?string $recipientLud06 = null;
+
+    public bool $isProtected = false;
+
+    public int $highlightCount = 0;
+
+    /** @var string[]|null */
+    public ?array $relays = null;
+}
